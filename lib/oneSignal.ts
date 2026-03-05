008fc58cb31d21abe49191a710437f0b70a2f38b
Jorge Rojano <jrojano@engageware.com>
2026-03-04 18:48:56 -0600

    remove notify button

--- lib/oneSignal.ts
+++ lib/oneSignal.ts
@@ -3,7 +3,6 @@ import OneSignal from "react-onesignal";
 export const initOneSignal = async () => {
   await OneSignal.init({
     appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID!,
-    notifyButton: { enable: false },
     allowLocalhostAsSecureOrigin: true,
   });
 };
